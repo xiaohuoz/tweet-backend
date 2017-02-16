a3f6c30613c0edecee6dbbbd0c1f1ada4db193fb
陈文辉1 <402909372@qq.com>
2017-02-16 18:00:16 +0800

    login

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,9 @@ var router = express.Router();
 var DB = require('../lib/Dao').DB;
 var user = DB('user');
 /* GET users listing. */
-router.route('/').post(function(req, res, next) {
+router.route('/login.do').post(function(req, res, next) {
 	var data = req.body;
 	console.log(data);
-	for(var key in  req.cookies){
-	    res.write("cookie名:"+key);
-	    res.write(",cookie值:"+req.cookies[key]+"<br />");
-	}
 	if(!(data.user_name && data.password)){
 		res.send('请输入用户名和密码');
 	}else{
@@ -30,5 +26,4 @@ router.route('/').post(function(req, res, next) {
 		})
 	}
 });
-
 module.exports = router;
